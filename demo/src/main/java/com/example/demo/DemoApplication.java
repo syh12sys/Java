4825ad489a10b42b495db7a674358130209a3baa
syh12sys <syh12sys@126.com>
2020-08-19 00:06:21 +0800

    profile区分环境配置

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,20 @@
 package com.example.demo;
 
+import io.shardingsphere.shardingjdbc.jdbc.core.datasource.ShardingDataSource;
+import io.shardingsphere.shardingjdbc.spring.boot.SpringBootConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.core.env.Environment;
 
-//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, SpringBootConfiguration.class})
+//@SpringBootApplication
 @ServletComponentScan("com.example.demo.filter")
 public class DemoApplication {
 
     public static void main(String[] args) {
+        System.out.println(System.getProperty("java.class.path"));
         SpringApplication.run(DemoApplication.class, args);
     }
 
--- demo/src/main/java/com/example/demo/config/DynamicDataSourceConfig.java
+++ demo/src/main/java/com/example/demo/config/DynamicDataSourceConfig.java
@@ -3,43 +3,46 @@ package com.example.demo.config;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
 
-//@Configuration
-//@PropertySource("file:G:/Java/demo/src/main/java/com/example/demo/config/jdbc.properties")
-//@MapperScan(basePackages = "com.example.demo.mapper")
-//public class DynamicDataSourceConfig {
-//    // TODO: Bean是有用的，只有使用了Bean注解，有效的DataSource才会被创建出来（包含配置）
-//    @Bean(value = "master")
-//    @ConfigurationProperties(prefix = "spring.datasource.mater")
-//    public DataSource masterDataSource() {
-//        return DataSourceBuilder.create().build();
-//    }
-//
-//    @Bean(value = "slave")
-//    @ConfigurationProperties(prefix = "spring.datasource.slave")
-//    public DataSource slaveDataSource() {
-//        return DataSourceBuilder.create().build();
-//    }
-//
-//    @Bean
-//    @Primary
-//    public DataSource dynamicDataSource() {
-//        Map<Object, Object> dataSources = new HashMap<>(2);
-//        //TODO: 这里不会创建新的对象，直接引用了Bean容器中的对象，为什么？
-//        dataSources.put("master", masterDataSource());
-//        dataSources.put("slave", slaveDataSource());
-//        // 设置动态数据源
-//        DynamicDataSource dynamicDataSource = new DynamicDataSource();
-//        dynamicDataSource.setTargetDataSources(dataSources);
-//        dynamicDataSource.setDefaultTargetDataSource(masterDataSource());
-//        return dynamicDataSource;
-//    }
-//}
+@Configuration
+@PropertySource(value= {"classpath:application-spring-datasource.properties"})
+@MapperScan(basePackages = "com.example.demo.mapper")
+// spring-datasource数据源此类才生效
+@Profile("spring-datasource")
+public class DynamicDataSourceConfig {
+    DynamicDataSourceConfig()
+    {
+    }
+
+    // TODO: Bean是有用的，只有使用了Bean注解，有效的DataSource才会被创建出来（包含配置）
+    @Bean(value = "master")
+    @ConfigurationProperties(prefix = "spring.datasource.mater")
+    public DataSource masterDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(value = "slave")
+    @ConfigurationProperties(prefix = "spring.datasource.slave")
+    public DataSource slaveDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @Primary
+    public DataSource dynamicDataSource() {
+        Map<Object, Object> dataSources = new HashMap<>(2);
+        //TODO: 这里不会创建新的对象，直接引用了Bean容器中的对象，为什么？
+        dataSources.put("master", masterDataSource());
+        dataSources.put("slave", slaveDataSource());
+        // 设置动态数据源
+        DynamicDataSource dynamicDataSource = new DynamicDataSource();
+        dynamicDataSource.setTargetDataSources(dataSources);
+        dynamicDataSource.setDefaultTargetDataSource(masterDataSource());
+        return dynamicDataSource;
+    }
+}
